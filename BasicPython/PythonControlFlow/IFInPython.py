3f0d78d2398aa51271aaf8ff427673f3fffc5bc5
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-04-04 00:15:53 +0300

    if else

---
+++ BasicPython/PythonControlFlow/IFInPython.py
@@ -0,0 +1,27 @@
+# if if is the statement that decided something in code
+# example one
+age = 14
+if age >= 18:
+    print('you can go what ever you went')
+print("with your family Please!")  # the output
+
+# if else
+
+num_age = 17
+
+if age <= 18:
+    print(f"you age is {age}  you can't buy any alcove")
+else:
+    print("you can buy what ever you went")
+
+# example 2 Odd or even
+num = int(input("Enter the number "))
+
+if num % 2 == 0:
+    print(f"the number is {num} is even")
+else:
+    print(f"the number is {num} is odd")
+
+# example
+
+
