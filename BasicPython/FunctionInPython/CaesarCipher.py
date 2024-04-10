e1ff20f4698b54ecbdb805f185fcc66197ad76c2
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-04-10 13:53:43 +0300

    end caesarCipher

--- BasicPython/FunctionInPython/CaesarCipher.py
+++ BasicPython/FunctionInPython/CaesarCipher.py
@@ -3,34 +3,73 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
             'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+
+#
+# def encrypt(planText, shiftNum):
+#     newWord = ''
+#     for position in planText:
+#         letter = alphabet.index(position)
+#         newPosition = letter + shiftNum
+#         newLetter = alphabet[newPosition]
+#         newWord += newLetter
+#     print(newWord)
+#
+#
+# def decrypt(cipherText, shiftNum):
+#     planText = ''
+#     for position in cipherText:
+#         letter = alphabet.index(position)
+#         newPosition = letter - shiftNum
+#         newLetter = alphabet[newPosition]
+#         planText += newLetter
+#     print(planText)
+#
+#
+# if direction == 'encode':
+#     encrypt(planText=text, shiftNum=shift)
+# elif direction == 'decode':
+#     decrypt(cipherText=text, shiftNum=shift)
 
 
-def encrypt(planText, shiftNum):
-    newWord = ''
-    for position in planText:
-        letter = alphabet.index(position)
-        newPosition = letter + shiftNum
-        newLetter = alphabet[newPosition]
-        newWord += newLetter
-    print(newWord)
+def CaesarCipher(edText, shiftNum):
+    if direction == 'encode':
+        newWord = ''
+        for position in edText:
+            if position in alphabet:
+                letter = alphabet.index(position)
+                newPosition = letter + shiftNum
+                newLetter = alphabet[newPosition]
+                newWord += newLetter
+            else:
+                newWord += position
+
+        print(newWord)
+    elif direction == 'decode':
+        planText = ''
+        for position in edText:
+            if position in alphabet:
+                letter = alphabet.index(position)
+                newPosition = letter - shiftNum
+                newLetter = alphabet[newPosition]
+                planText += newLetter
+            else:
+                planText += position
+
+        print(planText)
 
 
-def decrypt(cipherText, shiftNum):
-    planText = ''
-    for position in cipherText:
-        letter = alphabet.index(position)
-        newPosition = letter - shiftNum
-        newLetter = alphabet[newPosition]
-        planText += newLetter
-    print(planText)
+shouldContinue = True
 
+while shouldContinue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    CaesarCipher(edText=text, shiftNum=shift)
+    choose = input("Type 'yes' to Continue or 'No' to end the :\n").lower()
+    if choose == 'yes':
+        shouldContinue = True
+    elif choose == 'no':
+        shouldContinue = False
 
-if direction == 'encode':
-    encrypt(planText=text, shiftNum=shift)
-elif direction == 'decode':
-    decrypt(cipherText=text, shiftNum=shift)
-else:
-    print('Please enter appropriate word')
+    # CaesarCipher(edText=text, shiftNum=shift)
--- BasicPython/TestCode.py
+++ BasicPython/TestCode.py
@@ -1,16 +1,20 @@
-addName = True
-listOfName=[]
-while addName:
-    name = input('writ the name of the group:').split(',')
-    listOfName.append(name)
-    add = input("you when to add a name to countion 'Yes' to exit 'No").lower()
-    if add == 'yes':
-        name = input('writ the name of the group:')
-        listOfName.append(addName)
-
-    else:
-        addName = False
-print(f"list of student name: {listOfName}")
-# a=[1,2,3,4]
+# addName = True
+# listOfName=[]
+# while addName:
+#     name = input('writ the name of the group:').split(',')
+#     listOfName.append(name)
+#     add = input("you when to add a name to countion 'Yes' to exit 'No").lower()
+#     if add == 'yes':
+#         name = input('writ the name of the group:')
+#         listOfName.append(addName)
+#
+#     else:
+#         addName = False
+# print(f"list of student name: {listOfName}")
+# # a=[1,2,3,4]
 # x=a.index(4)
 # print(x)
+
+x=['jon','don']
+m=x.index('don')
+print(m)
